f0f16fa2e86889a9c74886e48a7c80737e56a6d0
hdsheena <hdsheena@gmail.com>
2015-09-04 15:05:02 -0700

    Cleaning up and putting in some function explanations

--- parsejoyride.py
+++ parsejoyride.py
@@ -1,5 +1,4 @@
 import json
-import sys
 from text_unidecode import unidecode 
 
 file = open('joyride2.json', 'rb')
@@ -17,84 +16,60 @@ def make_list_better(list_item):
     return better
     
     
-def put_rows_together():
-    for item in just_data:
-         #print item.keys()
-         try:
-            date = item['test']
-         except:
-            content = item['date']
-    #new_data[date] = content
 
-#put_rows_together()
 
 dates = []
 content = []
-for item in just_data:
-    if "test" in item.keys():
-        to_dates = item['test']
-        #print to_dates[0]
-        dates.append(to_dates[0])
-    else:
-        to_content = item['date']
+def create_lists_of_content_and_meta(just_data):
+    for item in just_data:
+        if "test" in item.keys():
+            to_dates = item['test']
+            #print to_dates[0]
+            dates.append(to_dates[0])
+        else:
+            to_content = item['date']
         
-        content.append(to_content[0])
-        
-new_content = []
-for item in content:
-    print "____________________________$$$$$_____________"
-    print item    
-    new_item = make_list_better(item)
-    
-    new_content.append(new_item)
-    
-content_variable = content[1]
-print content_variable
-content_variable_string = content_variable.encode('utf-8')
+            content.append(to_content[0])
 
-print content[0], "DATES", dates [0]
+new_content = []
+
+def make_content_strings_useable(content_list):
+        for item in content_list:
+            new_item = make_list_better(item)
+            new_content.append(new_item)
+    
 dictionary_of_metadata = {}
 list_of_threads = []
 list_of_post_numbers = []
-#make dates string into time stamp and topic heading
-for item in dates:
-    #print item
-    string = item
-    #print string
-    date_string = string.replace('#', 'to:')
-    #print "Splitting"
 
-    split_string = date_string.split('to:')
-    #print split_string[1], split_string[2]
-    name_of_thread = split_string[1][1:-1]
-    post_number = split_string[2]
-    #print name_of_thread, post_number
-    list_of_threads.append(name_of_thread)
-    list_of_post_numbers.append(post_number)
-    dictionary_of_metadata[name_of_thread] = post_number
+def make_nice_lists(dates_list):
+   """
+   make dates string into time stamp and topic heading
+   """
+   for item in dates_list:
+        string = item
+        date_string = string.replace('#', 'to:')
+        split_string = date_string.split('to:')
+        name_of_thread = split_string[1][1:-1]
+        post_number = split_string[2]
+        list_of_threads.append(name_of_thread)
+        list_of_post_numbers.append(post_number)
+        dictionary_of_metadata[name_of_thread] = post_number
 
 
 
 
 #print list_of_threads
     
-def make_new_dictionary():
+def make_new_dictionary(new_content, new_data):
     for item in new_content:
         new_value = {}
-        #print "ITEM", item
         number_of_item = new_content.index(item)
-        #print number_of_item
-        value = item
-        value_list = [item]
-        #print value_list
-        #print "VALUE", value
         key = list_of_threads[number_of_item]
         post_number = list_of_post_numbers[number_of_item]
-        new_value[post_number] = value
-        #print key
-        #print "TO DICTIONARY", new_value.values()
+        new_value[post_number] = item
         new_data.setdefault(key,[]).append(new_value)
-    return value_list
+    
 
 def thread_to_file(thread, key):
     #This doesn't work if the filename has a / in it without the if statement
@@ -105,16 +80,19 @@ def thread_to_file(thread, key):
     with open(filename, 'w') as outfile:
         json.dump(thread, outfile, encoding='utf-8')
 
-value_list = make_new_dictionary()
-for key in new_data.keys():
-    thread = new_data[key]
-    #print thread
-    thread_to_file(thread, key)
+def dictionary_to_files(new_data):
+    for key in new_data.keys():
+        thread = new_data[key]
+        thread_to_file(thread, key)
+    
+create_lists_of_content_and_meta(just_data)
+make_content_strings_useable(content)
+make_nice_lists(dates)
+make_new_dictionary(new_content, new_data)
+dictionary_to_files(new_data)
+
 #print dictionary_of_metadata
     
 print "           " *50
-    
-#better_string = make_list_better([content])
-print "_______________________"
-print better_string
+
 print new_content
